97be956cfcc7e79e69cd8ae55b7fad192a7101bb
Martynas <martynas@samesystem.dk>
2014-07-13 22:44:55 +0300

    Middle of the road while adding pictures

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,73 +1,62 @@
 class PicturesController < ApplicationController
+  before_action :set_article
   before_action :set_picture, only: [:show, :edit, :update, :destroy]
 
-  # GET /pictures
-  # GET /pictures.json
   def index
-    @pictures = Picture.all
+    @pictures = @article.pictures.ordered
   end
 
-  # GET /pictures/1
-  # GET /pictures/1.json
   def show
   end
 
-  # GET /pictures/new
   def new
     @picture = Picture.new
+    @picture.owner = @article
+    @articles.pictures.select("max(serial) as serial").first.serial.to_i + 1
   end
 
-  # GET /pictures/1/edit
   def edit
   end
 
-  # POST /pictures
-  # POST /pictures.json
   def create
     @picture = Picture.new(picture_params)
+    @picture.owner = @article
 
     respond_to do |format|
       if @picture.save
-        format.html { redirect_to @picture, notice: 'Picture was successfully created.' }
-        format.json { render :show, status: :created, location: @picture }
+        format.html { redirect_to article_pictures_path(@article), notice: 'Picture was successfully created.' }
       else
         format.html { render :new }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /pictures/1
-  # PATCH/PUT /pictures/1.json
   def update
     respond_to do |format|
       if @picture.update(picture_params)
-        format.html { redirect_to @picture, notice: 'Picture was successfully updated.' }
-        format.json { render :show, status: :ok, location: @picture }
+        format.html { redirect_to article_pictures_path(@article), notice: 'Picture was successfully updated.' }
       else
         format.html { render :edit }
-        format.json { render json: @picture.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /pictures/1
-  # DELETE /pictures/1.json
   def destroy
     @picture.destroy
     respond_to do |format|
-      format.html { redirect_to pictures_url, notice: 'Picture was successfully destroyed.' }
-      format.json { head :no_content }
+      format.html { redirect_to article_pictures_path(@article), notice: 'Picture was successfully destroyed.' }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_picture
-      @picture = Picture.find(params[:id])
+    def set_article
+      @picture = Article.find(params[:article_id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def set_picture
+      @picture = @article.pictures.where(id: params[:id])
+    end
+
     def picture_params
       params.require(:picture).permit(:title)
     end
--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,4 +1,5 @@
 class Picture < ActiveRecord::Base
   belongs_to :owner, polymorphic: true
+
   scope :ordered, -> { order('serial asc') }
 end
